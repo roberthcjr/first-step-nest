60ce70fe2e81036459f1f8abaaa3835ff9c867ce
Robert <roberthcjr1999@gmail.com>
2024-12-16 10:46:34 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: false,
+  });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
